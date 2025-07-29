a5739b90ec834c2227808c2e828459bed8ca6faa
Krishna Kumar <krishna7kandhan@gmail.com>
2025-07-29 14:11:27 +0530

    updated code

--- streamlite_practice.py
+++ streamlite_practice.py
@@ -6,11 +6,16 @@ import streamlit as st
 #Postgresql Database connection 			
 def db_connection():
 	try:			
-		db_conn_status = "postgresql://krishna:4YEGQ6WJavQHCDcyXmRH5ltplDbSZ3Bd@dpg-d1t45hmr433s73evet20-a.singapore-postgres.render.com/krishna_traffic"
+		#db_conn_status = "psycopg2://krishna:4YEGQ6WJavQHCDcyXmRH5ltplDbSZ3Bd@dpg-d1t45hmr433s73evet20-a.singapore-postgres.render.com/krishna_traffic"
+		db_conn_status = psycopg2.connect(database="krishna_traffic",
+                        host="dpg-d1t45hmr433s73evet20-a.singapore-postgres.render.com",
+                        user="krishna",
+                        password="4YEGQ6WJavQHCDcyXmRH5ltplDbSZ3Bd",
+                        port="5432")
 		return db_conn_status
 	except  Exception as db_conn_error:
-		db_conn_status=st.error(f"DATABASE CONNECTION FAILURE: {db_conn_error}")
-		return none
+		st.error(f"DATABASE CONNECTION FAILURE: {db_conn_error}")
+		return None
 	
 #Fecthing data from Connected Database
 def db_fetch_info(DB_QUERY):
@@ -21,14 +26,14 @@ def db_fetch_info(DB_QUERY):
 			cur.execute(query)
 			query_result = cur.fetchall()
 			result_df = pd.DataFrame(query_result)
-			cur.close()
+			#cur.close()
 			return result_df 
 		except Exception as db_error:
-			     result_df=st.error(f"Database error: {db_error}") 
-			     return result_df
+			st.error(f"Database error: {db_error}") 
+			return None
 	else:
 		st.error("Database Connection error") 
-		return pd.DataFrame()
+		return None
 
 #streamlite UI 
 st.set_page_config(page_title="GUVI-Krishna Kumar Traffic police logs insights project1", layout = "wide")
@@ -38,7 +43,7 @@ st.subheader("-developed by Krishna Kumar M")
 
 #Show full table 
 st.header("TRAFFIC POLICE LOGS - OVERVIEW")
-query = "select * from traffic_stops"
+query = "select * from traffic_stops where COUNTRY = 'INDIA'"
 result_data = db_fetch_info(query)
 #st.dataframe("Key Metrics")	
 st.dataframe(result_data)	
